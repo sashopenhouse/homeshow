62513845378bbebbfba87c7990600bc7ec3d142e
madeinutica <erick.florez@gmail.com>
2026-07-09 09:49:29 -0600

    Fix: Loaded correct high-res floor plan blueprint and loaded Leaflet CSS in root layout to fix missing polygons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "leaflet/dist/leaflet.css";
 import TransitionLayout from "@/components/animations/TransitionLayout";
 import Navbar from "@/components/Navbar";
 
